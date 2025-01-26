710949d31815e9f69e63435212001f73d6d39f47
suplicKonG <166747548+suplicKonG@users.noreply.github.com>
2025-01-26 17:23:12 +0100

    Update script.js

--- script.js
+++ script.js
@@ -5,15 +5,15 @@ function bmiCalculator (weight, height) {
 
 
     if (bmi <= 18.5){
-        alert("Your BMI is " + bmi + ", so you are underweight.");
+        alert("Your BMI is " + roundedBmi + ", so you are underweight.");
     }
 
     if (bmi > 18.5 && bmi <= 24.9){
-        alert("Your BMI is " + bmi + ", so you have a normal weight.");
+        alert("Your BMI is " + roundedBmi + ", so you have a normal weight.");
     }
 
     if (bmi > 24.9){
-        alert("Your BMI is " + bmi + ", so you are overweight.");
+        alert("Your BMI is " + roundedBmi + ", so you are overweight.");
     }
 }
 
